09c13633c59a209b9ae7f08cd830b1ffddab51ae
praneethaedara <praneethaedara@gmail.com>
2019-07-19 12:54:03 +0530

    cucumber file

--- src/test/java/org/tester/login/Cucumber/TestRunner.java
+++ src/test/java/org/tester/login/Cucumber/TestRunner.java
@@ -5,7 +5,7 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features= "src/main/java/org/tester/login/Cucumber/",tags = {}, strict = true, dryRun = false, glue="org.tester.login.Cucumber", monochrome = true, plugin="html:target")
+@CucumberOptions(features= "src/main/java/org/tester/login/Cucumber/",tags = {}, strict = true, dryRun = false, glue="org.tester.login.Cucumber", monochrome = true, plugin= {"html:target","json:target/cucumberReport.json"})
 
 public class TestRunner {
 
